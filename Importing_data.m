0a268628f44a8cca7db4c9afd0b813c168749b2a
kbokhari <kbokhari@uw.edu>
2016-02-26 14:23:52 -0800

    measles
    imports measles data

--- Importing_data.m
+++ Importing_data.m
@@ -1,2 +1,3 @@
 % Imports the data on health workforce numbers by country and year
 WF = readtable('health_workforce.xls');%%changed to WF rather than T
+M = readtable('measles.xls');%measles data by country
