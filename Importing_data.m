7f0353e75a75c23bd15fc07166ef8b8cabbd2041
kbokhari <kbokhari@uw.edu>
2016-02-29 23:24:36 -0800

    adds measles data
    graphs measles data

--- Importing_data.m
+++ Importing_data.m
@@ -1,2 +1,14 @@
+
 % Imports the data on health workforce numbers by country and year
 WF = readtable('health_workforce.xls');%%changed to WF rather than T
+
+% Imports the data on health workforce numbers by country and year
+T = readtable('xmart.csv')
+
+
+x= cell2mat(T{:,1});
+y=cell2mat(T{:,2:15});
+figure
+plot(x,y)
+
+
