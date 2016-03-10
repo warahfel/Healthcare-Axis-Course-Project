67a9afeca9756f538e0ebde263864879e740e0da
kbokhari <kbokhari@uw.edu>
2016-03-09 22:42:18 -0800

    3/9 updata
    everything done up until now

--- test.m
+++ test.m
@@ -43,10 +43,11 @@ title('Density of Physcians by Year')
 ylabel('Number of Countries')
 xlabel('Density of Physicians per 1000')
 
-
+%%%Stacked function? 
 %%code from http://www.mathworks.com/matlabcentral/answers/101869-can-hist-take-parameters-such-as-facecolor-and-edgecolor
 
 %%%Plot unsure about axes
+figure;
 plot(dp1,'.')
 hold on
 plot(dp5,'o')
@@ -66,7 +67,8 @@ Hall=H(2:117,2:5);%%Values of 1st 50 countries in list
 Hall=str2double(Hall);
 
 figure;
-hist(Hall)
+nbins = 1:.5:8;
+hist(Hall,nbins)
 legend ('2013','2009','2005','2001')
 title 'HIV Prevalence'
 xlabel 'Prevalence'
@@ -74,3 +76,27 @@ ylabel 'Number of Countries'
 
 
 
+%%%%% Workforce vs Prevalence
+
+%Scatter Plot
+WF1= WF(x2001,1);%country names for workforce 2001
+WF5=WF(x2005,1)
+WF9=WF(x2009,1)
+WF13=WF(x2013,1)
+
+h1=[.1,.1,.8,.6,27.7,.1,.1,.4,.4,.1,.6,.5,.1,25.5,.3,1.8,1.4,.1,.6,.3]'
+h5=[0.1, 0.1, 0.8, 25.4, 5.1, 5.2, 0.3, 0.5, 0.1, 2.4, 0.1, 0.6, 2.6, 1.3, 1.5, 0.9, 0.4, 0.1, 0.1 0.2, 0.8, 0.1, 3.8, 0.1, 0.6, 0.5, 0.1, 0.3, 0.1, 6.2, 0.4, 13.7]'
+h9=[0.1,2.0,0.1, 0.1,  0.1,0.3,1.6,23.6, 0.5, 0.9,4.8,4.9,0.3, 0.5,0.1, 0.1,0.2,0.4,  0.1, 1.6,0.1, 0.2,1.6, 0.6,3.9,1.,0.3, 0.3,6.,0.2,0.1,12.5,0.1, 1.1,0.2,0.1,0.1,0.7,0.2, 3.6, 0.1, 0.6,0.4,3.1, 0.1,0.4,0.1,26.8, 1.2,0.1,0.1, 0.2, 0.4, 0.1,13.2,16.5,0.1]'
+h13=[0.1, 0.2, 0.2, 0.5, 0.2, 0.3, 6.0, 0.2, 0.2, 0.6, 0.6, 19.1, 0.4, 0.3, 0.8, 0.3, 0.2, 0.4]'
+    
+    
+
+scatter(dp1,h1,'filled')
+hold on
+scatter(dp5,h5,'filled')
+scatter(dp9,h9,'filled')
+scatter(dp13,h13,'filled')
+
+xlabel('Physician Prevalence')
+ylabel('HIV Prevalence')
+title('Physician vs HIV Prevalence')
